926160d98618bffb295e5ee03c3079d0128071df
PyhteevIlya <112899541+PyhteevIlya@users.noreply.github.com>
2024-02-19 00:02:12 +0500

    Добавьте файлы проекта.

---
+++ ImageRecognitionMVC/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using DefectsMVC.Models;
+using ImageRecognition;
+using ImageRecognitionMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ML;
+using System.Diagnostics;
+using Tensorflow.Keras.Engine;
+
+namespace ImageRecognitionMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly PredictionEnginePool<MLImageModel.ModelInput, MLImageModel.ModelOutput> _predictionImage;
+
+        public HomeController(ILogger<HomeController> logger,
+            PredictionEnginePool<MLImageModel.ModelInput, MLImageModel.ModelOutput> predictionImage)
+        {
+            _logger = logger;
+            _predictionImage = predictionImage;
+        }
+
+        [HttpPost]
+        public IActionResult Upload(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("Файл не выбран.");
+            }
+
+            using MemoryStream ms = new MemoryStream();
+            file.OpenReadStream().CopyTo(ms);
+            var bytes = ms.ToArray();
+
+            MLImageModel.ModelInput sampleImage = new MLImageModel.ModelInput()
+            {
+                ImageSource = bytes
+            };
+
+            var predictImage = _predictionImage.Predict(sampleImage);
+            var labels = ModelLabel.GetLabels();
+
+            for (int i = 0; i < predictImage.Score.Length; i++)
+            {
+                var label = labels.FirstOrDefault(x => x.Id == i);
+
+                if (label != null) label.Score = (int)Math.Round(predictImage.Score[i] * 100);
+            }
+
+            labels = labels.OrderByDescending(x => x.Score).Take(2).ToList();
+
+            return Json(new { labels });
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormFile file)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(null);
+        }
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ImageRecognitionMVC/Models/ModelLabel.cs
@@ -0,0 +1,23 @@
+﻿namespace DefectsMVC.Models
+{
+    public class ModelLabel
+    {
+        public int Id { get; set; }
+
+        public string Label { get; set; }
+
+        public string Name { get; set; }
+
+        public int Score { get; set; }
+
+        public static List<ModelLabel> GetLabels()
+        {
+            return new List<ModelLabel>()
+                {
+                    new ModelLabel { Id = 0, Label = "Поясная складка ", Name = "waist folding" },
+                    new ModelLabel { Id = 1, Label = "Прокатная ямка", Name = "rolled_pit" },
+
+                };
+        }
+    }
+}
